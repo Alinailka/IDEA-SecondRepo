7625e2721aeeebd9889eda8148af37c93df46a91
Alinailka <alina-il@mail.ru>
2022-02-07 19:14:49 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int bonusMile = 20;
+
+
+        System.out.println("Вам начислены бонусные мили в размере:");
+        System.out.println ((ticketPrice / bonusMile) + " бонусных миль");
+    }}
